e1a5a072baed320247e0e07e0e3a2a2dbf1a3ccb
valerio-figueira <j.valerio.figueira@gmail.com>
2023-02-11 10:16:42 -0300

    class update

--- rubyClass.rb
+++ rubyClass.rb
@@ -17,8 +17,43 @@ class Person
 	def sleep
 		puts "#{@name} is zZzz just now. Dont disturb."
 	end
+
+	def name
+	  @name
+	end
+
+	def set_name=(name)
+	  @name = name
+	end
+	
+	def gender
+	  @gender
+	end
+
+	def set_gender=(gender)
+	  @gender = gender
+	end
+
+	def country
+	  @country
+	end
+
+	def set_country=(country)
+	  @country = country
+	end
+
+	def age
+	  @age
+	end
+
+	def set_age=(age)
+	  @age = age
+	end
 end
 
+def createPerson
+	person = Person.new("Ioannes", "man", "Brazilian", 74)
+	puts person.name, person.gender, person.country, person.age
+end
 
-
-
+createPerson
